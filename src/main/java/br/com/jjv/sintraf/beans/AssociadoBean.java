ae2a6cfd062d317908ec7bed5288a5b48a2608d8
jefferson <emanuelbassguitar@gmail.com>
2014-09-15 19:24:52 -0300

    Fotografia

--- src/main/java/br/com/jjv/sintraf/beans/AssociadoBean.java
+++ src/main/java/br/com/jjv/sintraf/beans/AssociadoBean.java
@@ -12,9 +12,6 @@ import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
-import javax.faces.bean.SessionScoped;
-import javax.faces.bean.ViewScoped;
-import org.primefaces.model.DefaultStreamedContent;
 import org.primefaces.model.StreamedContent;
 
 /**
