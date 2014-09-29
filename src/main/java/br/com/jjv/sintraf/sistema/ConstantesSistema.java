286c9be53ab5f26337575737d7dbc2e3eaa151f9
jefferson <emanuelbassguitar@gmail.com>
2014-09-29 17:06:38 -0300

    ??

--- src/main/java/br/com/jjv/sintraf/sistema/ConstantesSistema.java
+++ src/main/java/br/com/jjv/sintraf/sistema/ConstantesSistema.java
@@ -21,62 +21,10 @@ public class ConstantesSistema {
     public static final BigDecimal VALOR_MENSALIDADE = pegarValorMensalidade();
     public static final String JANEIRO_2014 = "";
     public static final String CAMINHO_IMAGEM = File.separator + "home"
-                + File.separator + "jefferson" + File.separator + "Área de Trabalho"
-                + File.separator + "Imagens" + File.separator + "Sintraf" + File.separator;
+            + File.separator + "jefferson" + File.separator + "Área de Trabalho"
+            + File.separator + "Imagens" + File.separator + "Sintraf" + File.separator;
     public static final String CAMINHO_ARQUIVO_CONFIGURACOES = "/home/jefferson/definicoes.xml";
 
-    public static BigDecimal pegarValorMensalidade() {
 
-        Sistema sistema = new Sistema();
-        
-        try {
-            XStream xStream = new XStream(new Dom4JDriver());
-            xStream.alias("sistema", Sistema.class);
-            xStream.processAnnotations(Sistema.class);
 
-            BufferedReader input = new BufferedReader(new FileReader(new File(CAMINHO_ARQUIVO_CONFIGURACOES)));
-
-            sistema = (Sistema) xStream.fromXML(input);
-
-            input.close();
-
-        } catch (IOException ex) {
-            
-            System.out.println("Não foi possível Ler o arquivo de configurações");
-        
-        }
-        
-        return sistema.getValorMensalidade();
-    }
-
-    
-    
-    
-            + File.separator + "vanderlan" + File.separator + "Área de Trabalho"
-            + File.separator + "imagens" + File.separator + "Sintraf" + File.separator;
-    public static final String CAMINHO_ARQUIVO_CONFIGURACOES = "/home/vanderlan/definicoes.xml";
-
-    public static BigDecimal pegarValorMensalidade() {
-
-        Sistema sistema = new Sistema();
-        
-        try {
-            XStream xStream = new XStream(new Dom4JDriver());
-            xStream.alias("sistema", Sistema.class);
-            xStream.processAnnotations(Sistema.class);
-
-            BufferedReader input = new BufferedReader(new FileReader(new File(CAMINHO_ARQUIVO_CONFIGURACOES)));
-
-            sistema = (Sistema) xStream.fromXML(input);
-
-            input.close();
-
-        } catch (IOException ex) {
-            
-            System.out.println("Não foi possível Ler o arquivo de configurações");
-        
-        }
-        
-        return sistema.getValorMensalidade();
-    }
 }
